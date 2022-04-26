1a3355090eb83ac2d7cff31b4ddd005a4a36cf54
Tawanda M. Nyoni <tmnyoni@outlook.com>
2022-04-26 23:07:08 +0200

    updated services pictures.

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -4,7 +4,7 @@ import styles from "../styles/styles.module.css";
 export default function Services() {
     return (
         <div
-            className="container col-xxl-8 px-4 py-5"
+            className="container col-xxl-8 my-4 px-4 py-5"
             id="services"
         >
             <h1 className="display-5 fw-bolder lh-1 mb-5 text-center">
@@ -15,25 +15,17 @@ export default function Services() {
                 <div className={`row ${styles.service}`}>
                     <div className="col-12 col-lg-6">
                         <img
-                            src="assets/profile.jpg"
+                            src="assets/beauty.png"
                             className="d-block mx-lg-auto my-5 my-md-auto img-fluid"
                             alt="Bootstrap Themes"
-                            width="400"
-                            height="300"
+                            width="600"
+                            height="500"
                             loading="lazy"
                         />
                     </div>
                     <div className="col-12 col-lg-6 d-flex align-items-center">
-                        <div>
-                            <img
-                                src="assets/1.png"
-                                alt="number-1"
-                                className="img-fluid"
-                            />
-                        </div>
-
                         <div className="ms-3">
-                            <h5> Is good looking website </h5>
+                            <h3> Is a good looking website </h3>
                             <p>
                                 A well-thought-out design will portray you as the
                                 expert and authority you know you
@@ -46,25 +38,17 @@ export default function Services() {
                 <div className={`row  flex-row-reverse ${styles.service}`}>
                     <div className="col-12 col-sm-12 col-lg-6">
                         <img
-                            src="assets/profile.jpg"
+                            src="assets/urban-scientists-studying-atom-by-the-computer.png"
                             className="d-block mx-lg-auto my-5 my-md-auto img-fluid"
                             alt="Bootstrap Themes"
-                            width="400"
-                            height="300"
+                            width="600"
+                            height="500"
                             loading="lazy"
                         />
                     </div>
                     <div className="col-12 col-lg-6 d-flex align-items-center">
-                        <div>
-                            <img
-                                src="assets/2.png"
-                                alt="number-2"
-                                className="img-fluid"
-                            />
-                        </div>
-
                         <div className="ms-3">
-                            <h5>Is a 27/4 website </h5>
+                            <h3> Is a 27/4 website </h3>
                             <p>
                                 A well-designed website with few faults  that disrupt
                                 your presence on the internet, hurting your market.
@@ -76,25 +60,17 @@ export default function Services() {
                 <div className={`row ${styles.service}`}>
                     <div className="col-12 col-sm-12 col-lg-6">
                         <img
-                            src="assets/profile.jpg"
+                            src="assets/urban-851.png"
                             className="d-block mx-lg-auto my-5 my-md-auto img-fluid"
                             alt="Bootstrap Themes"
-                            width="400"
-                            height="300"
+                            width="600"
+                            height="500"
                             loading="lazy"
                         />
                     </div>
                     <div className="col-12 col-lg-6 d-flex align-items-center">
-                        <div>
-                            <img
-                                src="assets/3.png"
-                                alt="number-3"
-                                className="img-fluid"
-                            />
-                        </div>
-
                         <div className="ms-3">
-                            <h5> Is a client reaching website </h5>
+                            <h3> Is a client reaching website </h3>
                             <p>
                                 Remember that your internet presence is important,
                                 so make it easy for your clients to identify you.
