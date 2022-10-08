5951c61db663dcd1e9df6a90db8ce26ad7909e99
Tawanda M <tmnyoni@outlook.com>
2022-10-08 21:17:05 +0200

    restyled layout.

--- pages/components/layout.tsx
+++ pages/components/layout.tsx
@@ -2,14 +2,16 @@ import React, { PropsWithChildren } from "react";
 import Navbar from "./navbar";
 import Footer from "./footer";
 
-const Layout = (props: PropsWithChildren) => {
-  return (
-    <div className="mx-2 md:mx-24 md:px-12">
-      <Navbar />
-      <main>{props.children}</main>
-      <Footer />
-    </div>
-  );
+const Layout = ({ children }: PropsWithChildren) => {
+    return (
+        <div className="mx-4 md:mx-auto md:w-10/12 lg:w-8/12">
+            <Navbar />
+            <main>
+                {children}
+            </main>
+            <Footer />
+        </div>
+    );
 };
 
 export default Layout;
