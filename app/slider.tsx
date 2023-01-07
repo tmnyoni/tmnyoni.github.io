e4d35f8578281770aa4cb65afa091fa0def08ad6
Tawanda M <tmnyoni@outlook.com>
2023-01-08 01:45:29 +0200

    carousel working on big screen.

--- app/slider.tsx
+++ app/slider.tsx
@@ -1,22 +1,22 @@
 export default function Slider() {
     return (
-        <div className={`bg-white shadow h-[100px] m-auto overflow-hidden relative w-[960px]
-        after:bg-gradient-to-r after:from-white after:to-white 
-        after:h-[100px] after:absolute after:w-[200px] after:z-[2] after:r-0 after:top-0 
-        transform rotate- before:bg-gradient-to-r before:from-white before:to-white 
-        before:h-[100px] before:absolute before:w-[200px] before:z-[2] before:top-0 before:left-0 before:bg-green-100`}
+        <div className={`hidden lg:block b-white shadow before:bg-gradient-to-r before:from-white before:to-white 
+        before:h-[100px] before:absolute before:z-[2] before:top-0 before:left-0 before:bg-green-100
+         h-[100px] m-auto overflow-hidden relative w-[960px] after:bg-gradient-to-r after:from-white after:to-white 
+        after:h-[100px] after:absolute after:z-[2] after:r-0 after:top-0 
+        transform`}
         >
-            <div className="animate-scroll flex w-[3000px] bg-red-100">
-                <div className="h-[100px] w-[300px] text-black text-5xl flex items-center justify-center bg-red-100"> C++ </div>
-                <div className="h-[100px] w-[300px] text-black text-5xl flex items-center justify-center bg-green-100"> TypeScript </div>
-                <div className="h-[100px] w-[300px] text-black text-5xl flex items-center justify-center bg-blue-100"> Java </div>
-                <div className="h-[100px] w-[300px] text-black text-5xl flex items-center justify-center bg-yellow-100"> Python </div>
-                <div className="h-[100px] w-[300px] text-black text-5xl flex items-center justify-center bg-gray-400"> JavaScript </div>
-                <div className="h-[100px] w-[300px] text-black text-5xl flex items-center justify-center bg-red-100"> C++ </div>
-                <div className="h-[100px] w-[300px] text-black text-5xl flex items-center justify-center bg-green-100"> TypeScript </div>
-                <div className="h-[100px] w-[300px] text-black text-5xl flex items-center justify-center bg-blue-100"> Java </div>
-                <div className="h-[100px] w-[300px] text-black text-5xl flex items-center justify-center bg-yellow-100"> Python </div>
-                <div className="h-[100px] w-[300px] text-black text-5xl flex items-center justify-center bg-gray-400"> JavaScript </div>
+            <div className="animate-scroll flex w-[3000px] text-5xl textwhite">
+                <div className="h-[100px] w-[300px] flex items-center justify-center bg-ed-100"> C++ </div>
+                <div className="h-[100px] w-[300px] flex items-center justify-center bg-reen-100"> TypeScript </div>
+                <div className="h-[100px] w-[300px] flex items-center justify-center bg-lue-100"> Java </div>
+                <div className="h-[100px] w-[300px] flex items-center justify-center bg-ellow-100"> Python </div>
+                <div className="h-[100px] w-[300px] flex items-center justify-center bg-ray-400"> JavaScript </div>
+                <div className="h-[100px] w-[300px] flex items-center justify-center bg-ed-100"> C++ </div>
+                <div className="h-[100px] w-[300px] flex items-center justify-center bg-reen-100"> TypeScript </div>
+                <div className="h-[100px] w-[300px] flex items-center justify-center bg-lue-100"> Java </div>
+                <div className="h-[100px] w-[300px] flex items-center justify-center bg-ellow-100"> Python </div>
+                <div className="h-[100px] w-[300px] flex items-center justify-center bg-ray-400"> JavaScript </div>
             </div>
         </div >
     )
