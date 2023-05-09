e92dc9f26a3a24075a28cbfffd55aa1f50baf117
Tawanda M. Nyoni <tmnyoni@outlook.com>
2023-05-09 16:09:55 +0200

    update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,55 @@ interface RootLayoutProps {
     children: React.ReactNode;
 }
 
+import { siteConfig } from "@/config/site";
+
+export const metadata = {
+     title: {
+      default: siteConfig.name,
+      template: `%s | ${siteConfig.name}`,
+    },
+    description: siteConfig.description,
+    keywords: [
+      "Next.js",
+      "Fullstack Developer",
+      "Frontend Developer",
+      "Software Developer",
+      "UI & UX Designer",
+    ],
+    authors: [
+      {
+        name: "tmnyoni",
+        url: "https://tmnyoni.ml",
+      },
+    ],
+    creator: "tmnyoni",
+    themeColor: [
+      { media: "(prefers-color-scheme: light)", color: "white" },
+      { media: "(prefers-color-scheme: dark)", color: "black" },
+    ],
+    openGraph: {
+      type: "website",
+      locale: "en_US",
+      url: siteConfig.url,
+      title: siteConfig.name,
+      description: siteConfig.description,
+      siteName: siteConfig.name,
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: siteConfig.name,
+      description: siteConfig.description,
+    //   images: [`${siteConfig.url}/og.jpg`],
+      creator: "@tmnyoni",
+    },
+    // icons: {
+    //   icon: "/favicon.ico",
+    //   shortcut: "/favicon-16x16.png",
+    //   apple: "/apple-touch-icon.png",
+    // },
+    // manifest: `${siteConfig.url}/site.webmanifest`,
+  }
+
 export default function RootLayout(props: RootLayoutProps) {
     const { children } = props;
 
---
+++ src/config/site.tsx
@@ -0,0 +1,8 @@
+import { SiteConfig } from "@/types";
+
+
+export const siteConfig: SiteConfig = {
+    name: "Tawanda M. Porfolio",
+    description: "Tawanda M. Porfolio",
+    url: "tmnyoni.ml",
+}
