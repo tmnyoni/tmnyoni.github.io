3391e66c1c7f2bfbbc02cd34f5cac432983391cb
Tawanda M. Nyoni <tmnyoni@outlook.com>
2023-08-03 11:10:17 +0200

    style: main layout styling

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -4,8 +4,10 @@ import Link from "next/link"
 export default function Page() {
     return (
         <main>
-            <div className="text-lg font-medium"> Hey, Im Tawanda M. </div>
-            <div className="max-w-[19ch] text-center text-[90px] font-black leading-none">
+            <div className="mb-10 text-center text-3xl font-medium">
+                Hey 👋, I&apos;m Tawanda M.
+            </div>
+            <div className="max-w-[19ch] text-center text-[90px] font-black leading-tight">
                 Software Developer and UI & UX Designer
             </div>
         </main>
