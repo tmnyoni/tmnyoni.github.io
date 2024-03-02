a37d05fdd0399cae4f4de82cc08496384cb82ecf
Tawanda M. Nyoni <tmnyoni@outlook.com>
2024-03-02 13:04:58 +0200

    updated the theme button

--- src/app/(site)/theme-button.tsx
+++ src/app/(site)/theme-button.tsx
@@ -1,20 +1,36 @@
 "use client"
 
-import { SunMoon } from "lucide-react"
+import { MoonIcon, SunIcon } from "lucide-react"
 import { useTheme } from "next-themes"
 
-import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
 
 export function ToggleThemeButton() {
     const { theme, setTheme } = useTheme()
     return (
-        <Button
-            className="absolute right-4 top-4 border"
+        <button
+            className="absolute right-4 top-4 inline-flex justify-between gap-x-1 rounded-full border border-gray-500 p-0.5 shadow-inner"
             onClick={() => {
                 setTheme(theme === "light" ? "dark" : "light")
             }}
         >
-            <SunMoon />
-        </Button>
+            <p className="sr-only">Theme</p>
+            <div
+                className={cn(
+                    "p-1",
+                    theme === "light" ? "" : "rounded-full bg-white text-black"
+                )}
+            >
+                <MoonIcon className="size-5" />
+            </div>
+            <div
+                className={cn(
+                    "p-1",
+                    theme === "dark" ? "" : "rounded-full bg-black text-white"
+                )}
+            >
+                <SunIcon className="size-5" />
+            </div>
+        </button>
     )
 }
